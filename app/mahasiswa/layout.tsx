fe280f0ac1e0cd9d9663ab9c075e49c58eed6323
zakyraihan <zakyraihan6336@gmail.com>
2024-06-15 10:23:56 +0700

    ed

--- app/mahasiswa/layout.tsx
+++ app/mahasiswa/layout.tsx
@@ -127,13 +127,13 @@ export default function MahasiswaLayout({ children }: { children: ReactNode }) {
         )}
       </div>
 
-      <footer className="footer footer-center p-10 bg-abu text-base-content rounded">
-        <nav className="grid grid-flow-col gap-4">
+      <div className="footer footer-center p-10 bg-abu text-base-content rounded">
+        <div className="grid grid-flow-col gap-4">
           <a className="link link-hover">About us</a>
           <a className="link link-hover">Contact</a>
           <a className="link link-hover">Jobs</a>
           <a className="link link-hover">Press kit</a>
-        </nav>
+        </div>
         <nav>
           <div className="grid grid-flow-col gap-4">
             <a>
@@ -174,7 +174,7 @@ export default function MahasiswaLayout({ children }: { children: ReactNode }) {
         <aside>
           <p>Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
         </aside>
-      </footer>
+      </div>
     </>
   );
 }
